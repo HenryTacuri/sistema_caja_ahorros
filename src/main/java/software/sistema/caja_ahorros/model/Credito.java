8ab7c473b69cdff427d8d3fb3564d1b9519a488c
FAndresG02 <andresfranklin02@gmail.com>
2024-12-05 02:59:41 -0500

    clase credito

---
+++ src/main/java/software/sistema/caja_ahorros/model/Credito.java
@@ -0,0 +1,81 @@
+package software.sistema.caja_ahorros.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Entity(name = "credito")
+public class Credito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "valor", nullable = false)
+    private double valor;
+
+    @Column(name = "fecha_acreditada", nullable = false)
+    private Date fechaAcreditada;
+
+    @Column(name = "fecha_liquidacion", nullable = false)
+    private Date fechaLiquidacion;
+
+    @ManyToOne
+    @JoinColumn(name = "cuenta_id")
+    private Cuenta cuenta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Date getFechaAcreditada() {
+        return fechaAcreditada;
+    }
+
+    public Date getFechaLiquidacion() {
+        return fechaLiquidacion;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setFechaAcreditada(Date fechaAcreditada) {
+        this.fechaAcreditada = fechaAcreditada;
+    }
+
+    public void setFechaLiquidacion(Date fechaLiquidacion) {
+        this.fechaLiquidacion = fechaLiquidacion;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Credito{" +
+                "id=" + id +
+                ", valor=" + valor +
+                ", fechaAcreditada=" + fechaAcreditada +
+                ", fechaLiquidacion=" + fechaLiquidacion +
+                ", cuenta=" + cuenta +
+                '}';
+    }
+}
